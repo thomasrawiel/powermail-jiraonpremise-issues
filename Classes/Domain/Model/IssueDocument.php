1a08cc832cfc680df3084f805f064a335eb91e8a
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2024-10-02 19:45:48 +0200

    [task] Format issue description

--- Classes/Domain/Model/IssueDocument.php
+++ Classes/Domain/Model/IssueDocument.php
@@ -3,34 +3,51 @@
 namespace TRAW\PowermailJiraonpremiseIssues\Domain\Model;
 
 use TRAW\PowermailJira\Domain\Model\IssueDocumentInterface;
+use TRAW\PowermailJira\Events\PowermailSubmitEvent;
 use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
+use In2code\Powermail\Domain\Model\Answer;
 
+/**
+ * Class IssueDocument
+ * @package TRAW\PowermailJiraonpremiseIssues\Domain\Model
+ */
 class IssueDocument implements IssueDocumentInterface
 {
-    public function getDescriptionForIssue(ObjectStorage $answers)
+    /**
+     * @param PowermailSubmitEvent $event
+     *
+     * @return string
+     */
+    public function getDescriptionForIssue(PowermailSubmitEvent $event)
     {
+        $uri = $event->getUri();
+        $url = $uri->getScheme() . '://' . $uri->getHost() . $uri->getPath();
+        $answers = $event->getMail()->getAnswers();
+
         $doc = '';
 
+        $nl = '
+        ';
+
         foreach ($answers->toArray() as $answer) {
-            $doc.='<p><strong>'.$answer->getField()->getTitle().'</strong></p>';
+            $doc .= $answer->getField()->getTitle() . ': ';
 
-//            switch ($answer->getValueType()) {
-//                case Answer::VALUE_TYPE_UPLOAD:
-//                case Answer::VALUE_TYPE_ARRAY:
-//                    foreach ($answer->getValue() as $uploadedFile) {
-//                        $doc->paragraph()->text($uploadedFile)->end();
-//                    }
-//                    break;
-//                default:
-//                    $doc->paragraph()->text($answer->getValue())->end();
-//            }
-
+            switch ($answer->getValueType()) {
+                case Answer::VALUE_TYPE_UPLOAD:
+                case Answer::VALUE_TYPE_ARRAY:
+                    foreach ($answer->getValue() as $uploadedFile) {
+                        $doc .= $uploadedFile;
+                    }
+                    break;
+                default:
+                    $doc .= $answer->getValue();
+            }
+            $doc .= $nl . $nl;
         }
 
-
-
-//        $doc->paragraph()->em('- - - This issue has been automatically created - - -')->end();
-//        $doc->paragraph()->em('URL: ' . $url)->end();
+        $doc .= $nl . $nl;
+        $doc .= '- - - This issue has been automatically created - - -' . $nl;
+        $doc .= 'URL: ' . $url;
 
         return $doc;
     }
